13b63961a274772465ad79c5e97d7c8a12995d36
Abhishek Chandra <abhichandra03@gmail.com>
2023-06-21 00:40:37 +0530

    wip

---
+++ backend/main.py
@@ -0,0 +1,25 @@
+from Adafruit_IO import Client
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+aio = Client("abhi1408", "aio_ZkLp75EMIrgEUOVYyjlCEvSytdgX")
+
+@app.route('/')
+def get():
+    feeds = aio.feeds()
+    data = {} 
+    for feed in feeds:
+        feed_key = feed[1]
+        values = aio.data(feed_key, max_results=50)
+        data[feed_key] = [[value[1].replace('T', ' ').replace('Z', ''), value[3]] for value in values]
+    resp = jsonify(data)
+    resp.status_code = 200
+    return resp
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+if __name__ == '__main__':
+    app.run(debug=True)
